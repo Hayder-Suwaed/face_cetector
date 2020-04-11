4356bd105f40c657cb782ac43efbb00a85a4ebd8
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-04-10 22:11:56 -0600

    added input text for detect faces in pictures

--- facedetector/src/App.js
+++ facedetector/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navigation from "./component/Navigation";
 import Logo from "./component/Logo/Logo";
+import ImageLinkForm from "./component/ImageLinkForm/ImageLinkForm";
 function App() {
   return (
     <div>
       <Navigation />
       <Logo />
+      <ImageLinkForm />
     </div>
   );
 }
